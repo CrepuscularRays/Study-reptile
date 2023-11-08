ec8a457a3f4fe30850b259893b2e85263ec161ef
156214593 <156214593@qq.com>
2023-11-08 18:59:47 +0800

    第一版

--- 搜狗页面爬取.py
+++ 搜狗页面爬取.py
@@ -1,12 +1,17 @@
 import requests
 
-# 1,指定url
-url = 'https://www.sogou.com/'
-# 2,发起请求get方法的返回值为响应对象
-response = requests.get(url=url)
-# 3，获取响应数据
-# .text：返回的是字符串形式的响应数据
+keyWord = input("enter a key word:")
+
+url = 'https://www.sogou.com/web'
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+}
+params = {"query": "周杰伦"}
+response = requests.get(url=url, headers=headers, params=params)
+response.encoding = "utf-8"
 page_text = response.text
-# 4，持久化存储
-with open('./sougou.html', 'w', encoding='utf-8') as fp:
+fileNmae = keyWord + ".html"
+with open(fileNmae, 'w', encoding='utf-8') as fp:
     fp.write(page_text)
+print("爬取完毕")
